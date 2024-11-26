351dc1f29e16de32ab2db98889ad9cf7dfc4a888
Jaylka Cristina <165946877+Jaylka@users.noreply.github.com>
2024-11-26 19:53:48 +0000

    operadores de Incremento e Decremento

--- operadores_++ e --.c
+++ operadores_++ e --.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+int main(){
+    /*
+    Incremento (++)
+    Pré-Incremento ++a
+    Pós-Incremento a++
+    Decremento (--)
+    Pré-Decremento --a
+    Pós-Decremento a--
+    */
+
+   int numero1 = 1;
+
+   printf("Antes Incremento: %d\n",numero1);
+   //aumenta 1unidade
+   numero1++,
+   printf("Após incremento: %d\n", numero1);
+
+}
