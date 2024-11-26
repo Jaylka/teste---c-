18f8155bacce27c38d8f1c3d104bc4c8857b34f6
Jaylka Cristina <165946877+Jaylka@users.noreply.github.com>
2024-11-26 19:54:13 +0000

    operadores de incremento e decremeto

--- operadores_++ e --.c
+++ operadores_++ e --.c
@@ -10,11 +10,31 @@ int main(){
     Pós-Decremento a--
     */
 
-   int numero1 = 1;
+    int numero1 = 1, resultado;
+    //Incremento
+    printf("Antes Incremento: %d\n",numero1);
+    // numero1 = numero1 + 1;
+    // numero1 += 1 (atribuição)
+    // Pós-incremento:
+    //resultado = numero1;  // atribuio valor, depois o incremento
+    //numero1++;
+    resultado = numero1++,
+    //printf("Após incremento: %d\n", numero1);
+    printf("Apos Pós-Icremento - Numero 1: %d - Resultado: %d\n", numero1,resultado);    //Decremento
+    
 
-   printf("Antes Incremento: %d\n",numero1);
-   //aumenta 1unidade
-   numero1++,
-   printf("Após incremento: %d\n", numero1);
+    //Pré-Incremento
+    resultado = ++numero1,
+    
+    
+    //printf("Após incremento: %d\n", numero1);
+    printf("Apos Pré-Icremento - Numero 1: %d - Resultado: %d\n", numero1,resultado);    
+    //numero1 -= 1(atribuição)
+    //numero1 = numero1 - 1;
+    numero1--,
+    printf("Após Decremento: %d\n", numero1);
+
+
+
 
 }
