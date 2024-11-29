ea7132f7e88a40db944740f4e1769d6511b1e342
Jaylka Cristina <165946877+Jaylka@users.noreply.github.com>
2024-11-29 18:38:56 +0000

    conversão implicita e explicita

--- conversao_implicita.c
+++ conversao_implicita.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    /*Implicita
+    /*Implicita: pode causar perda de dados
     
     int a = 10;
     float b = 3.5;
@@ -9,9 +9,9 @@ int main(){
 
     printf("Resultado: %.2f\n", resultado);
     */
-    //Explicita
+    //Explicita: permite +controle
     int a = 10;
-    int b = 3;
+    int b = 3;      //(float): casting que força o valor da variável
     float quociente = (float) a / b;  //"a" explicitamente convertido para float
 
     printf("Quociente: %.2f\n", quociente);
